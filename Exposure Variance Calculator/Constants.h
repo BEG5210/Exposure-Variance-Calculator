55db6cf488d970af5d8446210aec4931dc3b169d
105926570 <105926570@student.swin.edu.au>
2026-05-03 19:19:15 +1000

    added default values for camera settings

--- Exposure Variance Calculator/Constants.h
+++ Exposure Variance Calculator/Constants.h
@@ -4,3 +4,8 @@
 
 // Update once repURL changes [duh]
 const std::string repoUrl = "https://github.com/BEG5210/Exposure-Variance-Calculator.git";
+
+// Default camrea settings values. To be updated into the camera settings class.
+const float defaultShutterSpeed = 1.0f;
+const float defaultApeature = 1.0f;
+const int defaultIso = 100;
